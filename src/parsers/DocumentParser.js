e1b7ecf7742ac0b1415b10e5b9de0ae273836dd9
Gabriel McAdams <ghmcadams@yahoo.com>
2019-09-15 08:14:17 -0700

    Add support for json with comments

--- src/parsers/DocumentParser.js
+++ src/parsers/DocumentParser.js
@@ -13,12 +13,16 @@ export default class DocumentParser extends Parser {
         if (new.target === DocumentParser) {
             if (languages.match({ pattern: '**/.eslintrc.js', scheme: 'file', language: 'javascript' }, document) > 0) {
                 return new JSParser(document);
+            } else if (languages.match({ pattern: '**/.eslintrc.json', scheme: 'file', language: 'jsonc' }, document) > 0) {
+                return new JSONParser(document);
             } else if (languages.match({ pattern: '**/.eslintrc.json', scheme: 'file', language: 'json' }, document) > 0) {
                 return new JSONParser(document);
             } else if (languages.match({ pattern: '**/.eslintrc.yaml', scheme: 'file', language: 'yaml' }, document) > 0) {
                 return new YAMLParser(document);
             } else if (languages.match({ pattern: '**/.eslintrc.yml', scheme: 'file', language: 'yaml' }, document) > 0) {
                 return new YAMLParser(document);
+            } else if (languages.match({ pattern: '**/.eslintrc', scheme: 'file', language: 'jsonc' }, document) > 0) {
+                return new JSONParser(document);
             } else if (languages.match({ pattern: '**/.eslintrc', scheme: 'file', language: 'json' }, document) > 0) {
                 return new JSONParser(document);
             } else if (languages.match({ pattern: '**/.eslintrc', scheme: 'file', language: 'yaml' }, document) > 0) {
