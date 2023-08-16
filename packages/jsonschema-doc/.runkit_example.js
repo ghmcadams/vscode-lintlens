861572f65c6d73f6772068f769cd699988c04d9c
Gabriel McAdams <ghmcadams@yahoo.com>
2023-08-15 20:46:15 -0700

    [jsonschema-doc] runkit example

--- packages/jsonschema-doc/.runkit_example.js
+++ packages/jsonschema-doc/.runkit_example.js
@@ -1,3 +1,24 @@
-import { getSchemaDocumentation } from 'jsonschema-doc';
+const { getSchemaDocumentation } = require('jsonschema-doc');
+
+
+const schema = {
+  "type": "object",
+  "properties": {
+    "maxDepth": {
+      "oneOf": [
+        {
+          "type": "integer",
+          "minimum": 1
+        },
+        {
+          "type": "string",
+          "enum": ["∞"]
+        }
+      ]
+    }
+  },
+  "additionalProperties": false
+};
 
 const documentation = getSchemaDocumentation(schema);
+console.log(documentation);
